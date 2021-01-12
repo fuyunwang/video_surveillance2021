84ca4997d1e55c66d3c095d3ae9810981d166561
fuyunwang <2375872953@qq.com>
2021-01-12 14:54:48 +0800

    integrate aop

---
+++ surveillance-upms/surveillance-upms-api/src/main/java/com/fuyunwang/surveillance/upms/api/config/SurveillanceUpmsApiConfig.java
@@ -0,0 +1,12 @@
+package com.fuyunwang.surveillance.upms.api.config;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Description: 开启AOP
+ * @Author: FuyunWang
+ * @Date: 2021/1/12 14:51
+ */
+@EnableAspectJAutoProxy
+public class SurveillanceUpmsApiConfig {
+}
