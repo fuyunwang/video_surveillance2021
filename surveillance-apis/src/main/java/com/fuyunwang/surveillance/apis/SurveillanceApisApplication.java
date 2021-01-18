2af0adeeb5541f665a267f3e976461b88bb863fc
FuyunWang <beautifulsoup@163.com>
2021-01-18 21:02:36 +0800

    integrate leaf

--- surveillance-apis/src/main/java/com/fuyunwang/surveillance/apis/SurveillanceApisApplication.java
+++ surveillance-apis/src/main/java/com/fuyunwang/surveillance/apis/SurveillanceApisApplication.java
@@ -1,11 +1,13 @@
 package com.fuyunwang.surveillance.apis;
 
+import com.sankuai.inf.leaf.plugin.annotation.EnableLeafServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 //@RefreshScope
 @SpringBootApplication
+@EnableLeafServer
 public class SurveillanceApisApplication {
 
     public static void main(String[] args) {
---
+++ surveillance-apis/src/main/java/com/fuyunwang/surveillance/apis/controller/IdController.java
@@ -0,0 +1,29 @@
+package com.fuyunwang.surveillance.apis.controller;
+
+import com.sankuai.inf.leaf.common.Result;
+import com.sankuai.inf.leaf.service.SegmentService;
+import com.sankuai.inf.leaf.service.SnowflakeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: FuyunWang
+ * @Date: 2021/1/18 20:28
+ */
+@RestController
+public class IdController {
+
+    @Autowired(required = false)
+    private SegmentService segmentService;
+
+    @Autowired
+    private SnowflakeService snowflakeService;
+
+    @GetMapping("/segment")
+    public Result segment() {
+        return segmentService.getId("leaf-segment-test");
+    }
+}
